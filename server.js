a50bf787f94983467af8e03c41b722882f59a841
jmh233 <jiangminghao@westlake.onaliyun.com>
2023-08-10 16:18:30 +0800

    fix: 下载方法抽离

--- server.js
+++ server.js
@@ -30,19 +30,19 @@ let quality = 80; // 0-100 (100 为最高质量)
 const format = 'jpeg';
 
 async function compressImages(directory,  quality = 80) {
-  const imagePath = directory
   const compressedDirectory = path.join(directory, compressedSubdirectory);
   await createDirectoryIfNotExists(compressedDirectory);
 
-  const files = await fsPromises.readdir(imagePath)
+  const files = await fsPromises.readdir(directory)
   for (const file of files.filter(i => i !== compressedSubdirectory)) {
-    const filePath = path.join(imagePath, file);
+    const filePath = path.join(directory, file);
     const fileStats = await fsPromises.stat(filePath);
+    //单纯打印下文件信息
     console.log(`File: ${file}`);
     console.log('Size:', fileStats.size);
     console.log('Created at:', fileStats.birthtime);
     console.log('Modified at:', fileStats.mtime);
-    console.log('filePath', filePath)
+    console.log('filePath', filePath);
     console.log('-----------------------');
     const compressedFilePath = path.join(compressedDirectory, file);
     await sharp(filePath)
@@ -94,38 +94,46 @@ async function deleteDirectory(directory) {
   await fsPromises.rmdir(directory);
 }
 
+async function handleDownload(res, zipFilePath) {
+  try {
+    await new Promise((resolve, reject) => {
+      res.download(zipFilePath, (err) => {
+        if (err) reject(err);
+        else resolve();
+      });
+    });
+    await fsPromises.unlink(zipFilePath);
+  } catch (err) {
+    console.error('下载文件出错:', err);
+    res.status(500).send('下载文件出错');
+  }
+}
+
 app.post('/upload', upload.single('file'), async (req, res) => {
-  console.log(req.body, 'req.body---->')
   quality = req.body.quality;
   const outputDir = path.join(__dirname, extractedDirectory);
-  const zipFilePath = path.resolve(__dirname, outputZipFilename)
+  const zipFilePath = path.resolve(__dirname, outputZipFilename);
   try {
     await extract(req.file.path, { dir: outputDir })
     const files = await fsPromises.readdir(outputDir)
     //特殊情况处理压缩一个带图片的文件夹
     const filePath = path.join(outputDir, files[0]);
     const fileStats = await fsPromises.stat(filePath);
-    console.log(fileStats.isDirectory(), 'fileStats---->')
-    const dirName = files.find((i) => i !== 'compressed')
-    console.log(dirName, 'dirName---->')
+    const dirName = files.find((i) => i !== 'compressed');
+    console.log(dirName, '----dirName----')
     //直接选择图片压缩成一个zip包的地址outputDir
     const compressedDirectory = fileStats.isDirectory() ? (outputDir + dirName) : outputDir;
+    //压缩图片
     await compressImages(compressedDirectory, Number(quality));
+    //创建压缩包
     await createZip(path.join(compressedDirectory, compressedSubdirectory));
-    console.log('__dirname', __dirname)
-    res.download(zipFilePath, async (err) => {
-      if (err) {
-        console.error('下载文件出错:', err)
-        res.status(500).send('下载文件出错');
-      } else {
-        fsPromises.unlink(zipFilePath)
-      }
-    })
+    //下载压缩包并删除本地文件
+    await handleDownload(res, zipFilePath);
   } catch (err) {
     console.error('系统错误:', err);
     res.status(500).send('系统错误');
   } finally {
-    await deleteDirectory(outputDir)
+    await deleteDirectory(outputDir);
   }
 });
 
